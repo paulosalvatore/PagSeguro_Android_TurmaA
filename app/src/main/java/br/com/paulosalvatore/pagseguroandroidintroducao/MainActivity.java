66def81147a6d5a1ab4fd179dc0595831a24430a
Paulo Salvatore <paulo@salvatore.tech>
2019-02-18 13:00:57 -0300

    Correção do exercício: capturando eventos e manipulando elementos Internacionalização do app

--- app/src/main/java/br/com/paulosalvatore/pagseguroandroidintroducao/MainActivity.java
+++ app/src/main/java/br/com/paulosalvatore/pagseguroandroidintroducao/MainActivity.java
@@ -1,8 +1,10 @@
 package br.com.paulosalvatore.pagseguroandroidintroducao;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,10 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final TextView tvPrincipal = findViewById(R.id.tvPrincipal);
-        tvPrincipal.setOnClickListener(new View.OnClickListener() {
+        final EditText etPrincipal = findViewById(R.id.etPrincipal);
+        final Button btEnviar = findViewById(R.id.btEnviar);
+
+        btEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvPrincipal.setText("Novo Texto");
+                tvPrincipal.setText(etPrincipal.getText().toString());
             }
         });
     }
