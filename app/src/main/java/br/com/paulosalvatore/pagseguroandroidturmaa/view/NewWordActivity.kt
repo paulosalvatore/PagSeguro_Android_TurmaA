aa33f8a14844e42bc9bc1134c5a1b84bfb6340dd
Paulo Salvatore <paulo@salvatore.tech>
2019-03-28 10:26:45 -0300

    Aula 11 iniciada

---
+++ app/src/main/java/br/com/paulosalvatore/pagseguroandroidturmaa/view/NewWordActivity.kt
@@ -0,0 +1,13 @@
+package br.com.paulosalvatore.pagseguroandroidturmaa.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.paulosalvatore.pagseguroandroidturmaa.R
+
+class NewWordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+    }
+}
